44164c556ddb5a4f46bda07312feb4e90b17bae5
feilong <614183595@qq.com>
2016-08-03 09:13:00 +0800

    cmdtest

---
+++ cmdtest/cmdtest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace cmdtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Thread.Sleep(100000);
+        }
+    }
+}
